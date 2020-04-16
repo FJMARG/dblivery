43b509a4917e07527498a02671ab01883e2880bc
Federico Margueliche <federicomargueliche@gmail.com>
2020-04-16 20:12:43 -0300

    Se quito un paquete no utilizado.

--- bd2-2020/src/main/java/ar/edu/unlp/info/bd2/config/HibernateConfiguration.java
+++ bd2-2020/src/main/java/ar/edu/unlp/info/bd2/config/HibernateConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
