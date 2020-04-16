4221800ed1ed61ff16ce5bed946d8ee478f87a07
Federico Margueliche <federicomargueliche@gmail.com>
2020-04-16 03:18:38 -0300

    Se comenzaron a agregar las anotaciones @Transational a cada metodo del servicio. Aun falta checkear lo del entity manager, el getTransaction() y el commit() en el Repository

--- bd2-2020/src/main/java/ar/edu/unlp/info/bd2/services/DBliveryServiceImpl.java
+++ bd2-2020/src/main/java/ar/edu/unlp/info/bd2/services/DBliveryServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import ar.edu.unlp.info.bd2.model.*;
 import ar.edu.unlp.info.bd2.repositories.DBliveryException;
 import ar.edu.unlp.info.bd2.repositories.DBliveryRepository;
@@ -23,6 +25,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier) {
 		Product product = repository.getProductByName(name);
 		if( product == null) {
@@ -36,6 +39,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 	
 	@Override
+	@Transactional
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier, Date sdf) { // Problema entre test 1 y 2
 		//Product product = repository.getProductByName(name);
 		//if( product == null) {
@@ -49,6 +53,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 		
 
 	@Override
+	@Transactional
 	public Supplier createSupplier(String name, String cuil, String address, Float coordX, Float coordY) {
 		Supplier newSupplier = new Supplier(name, cuil, address, coordX, coordY);
 		
@@ -56,6 +61,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public User createUser(String email, String password, String username, String name, Date dateOfBirth) {
 		User user = repository.getUserByUsername(username);
 		if (user != null) {
@@ -68,6 +74,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Product updateProductPrice(Long id, Float price, Date startDate) throws DBliveryException {
 		Product product = repository.get(id, Product.class);
 		if (product == null) {
@@ -80,6 +87,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order createOrder(Date dateOfOrder, String address, Float coordX, Float coordY, User client) {
 		Order newOrder = new Order(client, coordX, coordY, address, dateOfOrder);
 //		busco el estado pending
@@ -97,6 +105,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 	
 	@Override
+	@Transactional
 	public Order addProduct(Long order, Long quantity, Product product) throws DBliveryException {
 		Product p = repository.get(product.getId(), Product.class);
 		Order updatedOrder = repository.get(order, Order.class);
@@ -113,36 +122,42 @@ public class DBliveryServiceImpl implements DBliveryService {
 	
 	
 	@Override
+	@Transactional
 	public Optional<User> getUserById(Long id) {
 		User u = repository.get(id, User.class);
 		return Optional.of(u);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserByEmail(String email) {
 		User u = repository.getUserByEmail(email);
 		return Optional.of(u);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserByUsername(String username) {
 		User u = repository.getUserByUsername(username);
 		return Optional.of(u);
 	}
 
 	@Override
+	@Transactional
 	public Optional<Product> getProductById(Long id) {
 		Product product = repository.get(id, Product.class);
 		return Optional.of(product);
 	}
 
 	@Override
+	@Transactional
 	public Optional<Order> getOrderById(Long id) {
 		Order order = repository.get(id, Order.class);
 		return Optional.of(order);
 	}
 	
 	@Override
+	@Transactional
 	public OrderStatus getActualStatus(Long order) {
 		Optional<Order> orderDB = this.getOrderById(order);
 		if(orderDB == null) 
@@ -152,12 +167,14 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductByName(String name) {
 		List<Product> productList = repository.getProductsByName(name);
 		return productList;
 	}
 	
 	@Override
+	@Transactional
 	public boolean canCancel(Long order) throws DBliveryException {
 		Optional<Order> orderDB = this.getOrderById(order);
 		if(orderDB == null) 
@@ -167,6 +184,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public boolean canFinish(Long id) throws DBliveryException {
 		Optional<Order> orderDB = this.getOrderById(id);
 		if(orderDB == null) 
@@ -176,6 +194,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public boolean canDeliver(Long order) throws DBliveryException {
 		Optional<Order> orderDB = this.getOrderById(order);
 		if(orderDB == null) 
@@ -185,6 +204,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 	
 	@Override
+	@Transactional
 	public Order deliverOrder(Long order, User deliveryUser) throws DBliveryException {
 		Order orderDB = repository.get(order, Order.class);
 		User userDB   = repository.get(deliveryUser.getId(), User.class);
@@ -207,6 +227,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 	
 	@Override
+	@Transactional
 	public Order deliverOrder(Long order, User deliveryUser, Date sdf) throws DBliveryException {
 		Order orderDB = repository.get(order, Order.class);
 		User userDB   = repository.get(deliveryUser.getId(), User.class);
@@ -229,6 +250,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order cancelOrder(Long order) throws DBliveryException {
 		Order orderDB = repository.get(order, Order.class);
 		
@@ -245,6 +267,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 	
 	@Override
+	@Transactional
 	public Order cancelOrder(Long order, Date sdf) throws DBliveryException {
 		Order orderDB = repository.get(order, Order.class);
 		
@@ -261,6 +284,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order finishOrder(Long order) throws DBliveryException {
 		Order orderDB = repository.get(order, Order.class);
 		
@@ -277,6 +301,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 	
 	@Override
+	@Transactional
 	public Order finishOrder(Long order, Date sdf) throws DBliveryException {
 		Order orderDB = repository.get(order, Order.class);
 		
@@ -294,6 +319,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getAllOrdersMadeByUser(String username) {
 		
 		Optional<User> userDB = this.getUserByUsername(username);
@@ -307,12 +333,14 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<User> getUsersSpendingMoreThan(Float amount) {
 		return repository.getUsersSpendingMoreThan(amount);
 	}
 
 
 	@Override
+	@Transactional
 	public List<Supplier> getTopNSuppliersInSentOrders(int n) {
 		// TODO Auto-generated method stub
 		return null;
@@ -320,6 +348,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Product> getTop10MoreExpensiveProducts() {
 		List<Product> productList = this.repository.getTop10MoreExpensiveProducts();
 		return productList;
@@ -327,6 +356,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<User> getTop6UsersMoreOrders() {
 		// TODO Auto-generated method stub
 		return null;
@@ -334,6 +364,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getCancelledOrdersInPeriod(Date startDate, Date endDate) {
 		// TODO Auto-generated method stub
 		return null;
@@ -341,6 +372,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getPendingOrders() {
 		// TODO Auto-generated method stub
 		return null;
@@ -348,6 +380,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getSentOrders() {
 		// TODO Auto-generated method stub
 		return null;
@@ -355,6 +388,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersInPeriod(Date startDate, Date endDate) {
 		// TODO Auto-generated method stub
 		return null;
@@ -362,6 +396,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersForUser(String username) {
 		// TODO Auto-generated method stub
 		return null;
@@ -369,6 +404,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getSentMoreOneHour() {
 		// TODO Auto-generated method stub
 		return null;
@@ -376,6 +412,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersSameDay() {
 		// TODO Auto-generated method stub
 		return null;
@@ -383,6 +420,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<User> get5LessDeliveryUsers() {
 		// TODO Auto-generated method stub
 		return null;
@@ -390,6 +428,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public Product getBestSellingProduct() {
 		Product p = this.repository.getBestSellingProduct();
 		return p;
@@ -397,6 +436,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Product> getProductsOnePrice() {
 		List<Product> list = this.repository.getProductsOnePrice();
 		// TODO Auto-generated method stub
@@ -405,6 +445,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Product> getProductIncreaseMoreThan100() {
 		// TODO Auto-generated method stub
 		return null;
@@ -412,6 +453,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public Supplier getSupplierLessExpensiveProduct() {
 		// TODO Auto-generated method stub
 		return null;
@@ -419,6 +461,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Supplier> getSuppliersDoNotSellOn(Date day) {
 		// TODO Auto-generated method stub
 		return null;
@@ -426,6 +469,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Product> getSoldProductsOn(Date day) {
 		List<Product> list = this.repository.getSoldProductsOn(day);
 		return list;
@@ -433,6 +477,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getOrdersCompleteMorethanOneDay() {
 		// TODO Auto-generated method stub
 		return null;
@@ -440,6 +485,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Object[]> getProductsWithPriceAt(Date day) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		return this.repository.getProductsWithPriceAt(sdf.format(day));
@@ -447,6 +493,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Product> getProductsNotSold() {
 		List<Product> list = this.repository.getProductsNotSold();
 		return list;
@@ -454,6 +501,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 
 
 	@Override
+	@Transactional
 	public List<Order> getOrderWithMoreQuantityOfProducts(Date day) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		ArrayList<Order> r = new ArrayList<Order>();
